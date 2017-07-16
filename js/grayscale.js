2fb3257a2ee11751459570b0539a169c8878c817
VictoriaVasys <vvasys@gmail.com>
2017-07-16 11:05:22 -0600

    Add education & skills

--- js/grayscale.js
+++ js/grayscale.js
@@ -21,7 +21,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top + 130
+            scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
